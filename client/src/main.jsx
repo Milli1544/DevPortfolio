3334712abde7b101e812840f1097d35d28df0d99
MilyonKifleyesus <mkifleye@my.centennialcollege.ca>
2025-08-04 17:05:41 -0400

    Update portfolio project with latest changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import "./index.css";
@@ -8,5 +9,6 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <ErrorBoundary>
     <App />
+    <SpeedInsights />
   </ErrorBoundary>
 );
